6c063b7b0bce8bc3aa2c4f5b53eaa102347d2001
Mohammad Azam <azamsharp@gmail.com>
2021-02-13 14:08:08 -0600

    Initial Commit

---
+++ IntroToSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntroToSwiftUI
+//
+//  Created by Mohammad Azam on 2/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IntroToSwiftUI/IntroToSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToSwiftUIApp.swift
+//  IntroToSwiftUI
+//
+//  Created by Mohammad Azam on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroToSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
